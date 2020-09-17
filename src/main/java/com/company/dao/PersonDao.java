00003f3327176d65bf124b5e12b0ec8b91fc2b5a
Ерофеевский Юрий Александрович <borlok@bk.ru>
2020-09-17 22:51:18 +0300

    +controller-create, html-new

--- src/main/java/com/company/dao/PersonDao.java
+++ src/main/java/com/company/dao/PersonDao.java
@@ -9,15 +9,16 @@ import java.util.List;
 
 @Component
 public class PersonDao {
+    private int ID_COUNT;
     private List<Person> people;
 
     {
         people = new ArrayList<>();
 
-        people.add(new Person(1,"Petr"));
-        people.add(new Person(2,"Alex"));
-        people.add(new Person(3,"Victor"));
-        people.add(new Person(3,"Katy"));
+        people.add(new Person(++ID_COUNT,"Petr"));
+        people.add(new Person(++ID_COUNT,"Alex"));
+        people.add(new Person(++ID_COUNT,"Victor"));
+        people.add(new Person(++ID_COUNT,"Katy"));
     }
 
     public List<Person> index () {
@@ -26,5 +27,10 @@ public class PersonDao {
 
     public Person show(int id) {
         return people.stream().filter(x->x.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++ID_COUNT);
+        people.add(person);
     }
 }
